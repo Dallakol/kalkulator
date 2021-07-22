824f8412b18b3cd826cf9d286e19d76666665ccc
Sebastian Chmielewski <1997chmiel@gmail.com>
2021-07-22 15:20:51 +0200

    Poprawka kalkulatora.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,5 @@
 #Dodawanie
-def add (a,b,):
+def add (a,b):
     return(a + b)
 #Odejmowanie
 def subtract (a,b):
@@ -24,17 +24,20 @@ if choice in ('1', '2', '3', '4'):
     num2 = float(input("Podaj drugą wartość: "))
 
     if choice == '1':
-        print(num1, '+', num2, '=', add(num1, num2))
+        print("Dodaję", num1, "i", num2)
+        print("Wynik to: ", add(num1,num2))
 
     elif choice== '2':
-        print(num1, '-', num2, '=', subtract(num1,num2))
+        print("Odejmuję", num1, "od", num2)
+        print("Wynik to: ", subtract(num1,num2))
 
     elif choice == '3':
-        print(num1, '*', num2, '=', multiply(num1,num2))
+        print("Mnożę", num1, "przez", num2)
+        print("Wynik to: ", multiply(num1,num2))
 
     elif choice == '4':
-        print(num1, '/' , num2, '=', divide(num1,num2))
+        print("Dzielę", num1, "przez", num2)
+        print("Wynik to: ", divide(num1,num2))
 #Jeśli nie wybierze liczb 1-4
 else:
     print("Zły wybór")
-    
